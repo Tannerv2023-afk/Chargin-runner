b0da851496d523373bbfe6a545908e0ce9f89b05
Tannerv2023-afk <140565908+Tannerv2023-afk@users.noreply.github.com>
2023-07-26 15:42:05 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,92 @@
+let mySprite = sprites.create(img`
+    . . . . 2 2 2 2 2 e . . . . . . 
+    . . . 2 2 2 2 d 2 2 e . . . . . 
+    . . e 2 2 2 2 2 2 2 e . . . . . 
+    . . e 2 2 2 2 2 2 2 e . . . . . 
+    . . e 2 2 2 2 2 e f f c c . . . 
+    . . e e 2 2 e f f f f b c . . . 
+    . e e e f e 2 b f f f d c . . . 
+    e e 2 2 d f 2 1 1 1 1 b c . . . 
+    e e 2 2 d f e e c c c . . . . . 
+    b 1 1 d e 2 2 e e c . . . . . . 
+    . f f e 2 2 2 2 e . . . . . . . 
+    . . f f d d 2 2 f f d d . . . . 
+    . . f f d d e e f f d d . . . . 
+    . . . f f f f . . . . . . . . . 
+    . . e e e f f f . . . . . . . . 
+    . . e e e e f f f . . . . . . . 
+    `, SpriteKind.Player)
+characterAnimations.loopFrames(
+mySprite,
+[img`
+    . . . . 2 2 2 2 2 e . . . . . . 
+    . . . 2 2 2 2 d 2 2 e . . . . . 
+    . . e 2 2 2 2 2 2 2 e . . . . . 
+    . . e 2 2 2 2 2 2 2 e . . . . . 
+    . . e 2 2 2 2 2 e f f c c . . . 
+    . . e e 2 2 e f f f f b c . . . 
+    . e e e f e 2 b f f f d c . . . 
+    e e 2 2 d f 2 1 1 1 1 b c . . . 
+    e e 2 2 d f e e c c c . . . . . 
+    b 1 1 d e 2 2 e e c . . . . . . 
+    . f f e 2 2 2 2 e . . . . . . . 
+    . . f f d d 2 2 f f d d . . . . 
+    . . f f d d e e f f d d . . . . 
+    . . . f f f f . . . . . . . . . 
+    . . e e e f f f . . . . . . . . 
+    . . e e e e f f f . . . . . . . 
+    `,img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . 2 2 2 2 2 e . . . . 
+    . . . . . 2 2 2 2 d 2 2 e . . . 
+    . . . . e 2 2 2 2 2 2 2 e . . . 
+    . . . . e 2 2 2 2 2 2 2 e . . . 
+    . . . . e 2 2 2 2 2 e f f c c . 
+    . . . . e e 2 2 e f f f f b c . 
+    . . . e e e f e 2 b f f f d c . 
+    . . e e 2 2 d f 2 1 1 1 1 b c . 
+    . . e e 2 2 d f e e c c c . . . 
+    . . b 1 1 d e 2 e e c . . . . . 
+    . . f f f f d d f . . . . . . . 
+    f f f f f f d d . . . . . . . . 
+    f f f . f f f e . . . . . . . . 
+    f f . . . . e e e . . . . . . . 
+    . . . . . . e e e e . . . . . . 
+    `,img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . 2 2 2 2 2 e . . . . 
+    . . . . . 2 2 2 2 d 2 2 e . . . 
+    . . . . e 2 2 2 2 2 2 2 e . . . 
+    . . . . e 2 2 2 2 2 2 2 e . . . 
+    . . . . e 2 2 2 2 2 e f f c c . 
+    . . . . e e 2 2 e f f f f b c . 
+    . . e e e f e 2 2 b f f f d c . 
+    . e e 2 2 d f e 2 1 1 1 1 b c . 
+    . e e 2 2 d f e e e c c c . . . 
+    . b 1 1 e e 2 2 e e c . . . . . 
+    . . f d d 2 2 2 f f f d d . . . 
+    e e f d d e e e . f f d d . . . 
+    e e e f f f f f . . . . . . . . 
+    e e . . . . f f f . . . . . . . 
+    . . . . . . f f f f . . . . . . 
+    `,img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . 2 2 2 2 2 e . . . 
+    . . . . . . 2 2 2 2 d 2 2 e . . 
+    . . . . . e 2 2 2 2 2 2 2 e . . 
+    . . . . . e 2 2 2 2 2 2 2 e . . 
+    . . . . . e 2 2 2 2 2 e f f c c 
+    . . . . . e e 2 2 e f f f f b c 
+    . . . e e e f e 2 2 b f f f d c 
+    . . e e 2 2 d f e 2 1 1 1 1 b c 
+    . . e e 2 2 d f e e e c c c . . 
+    . . b 1 1 d e 2 2 e e c . . . . 
+    . . . f f f d d 2 2 f d d . . . 
+    . . . . f f d d e e f d d . . . 
+    . . . . . f f f f f . . . . . . 
+    . . . . e e e f f . . . . . . . 
+    . . . . e e e e f f . . . . . . 
+    `],
+100,
+characterAnimations.rule(Predicate.NotMoving)
+)
