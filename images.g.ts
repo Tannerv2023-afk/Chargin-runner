f4bc0d3a947b21d4f14aaaa6bb0f2d1fd4cbb326
Tannerv2023-afk <140565908+Tannerv2023-afk@users.noreply.github.com>
2023-07-27 15:22:23 -0700

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,114 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Runner":return img`
+. . . . 2 2 2 2 2 e . . . . . . 
+. . . 2 2 2 2 d 2 2 e . . . . . 
+. . e 2 2 2 2 2 2 2 e . . . . . 
+. . e 2 2 2 2 2 2 2 e . . . . . 
+. . e 2 2 2 2 2 e f f c c . . . 
+. . e e 2 2 e f f f f b c . . . 
+. e e e f e 2 b f f f d c . . . 
+e e 2 2 d f 2 1 1 1 1 b c . . . 
+e e 2 2 d f e e c c c . . . . . 
+b 1 1 d e 2 2 e e c . . . . . . 
+. f f e 2 2 2 2 e . . . . . . . 
+. . f f d d 2 2 f f d d . . . . 
+. . f f d d e e f f d d . . . . 
+. . . f f f f . . . . . . . . . 
+. . e e e f f f . . . . . . . . 
+. . e e e e f f f . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "Running":
+            case "anim1":return [img`
+. . . . 2 2 2 2 2 e . . . . . . 
+. . . 2 2 2 2 d 2 2 e . . . . . 
+. . e 2 2 2 2 2 2 2 e . . . . . 
+. . e 2 2 2 2 2 2 2 e . . . . . 
+. . e 2 2 2 2 2 e f f c c . . . 
+. . e e 2 2 e f f f f b c . . . 
+. e e e f e 2 b f f f d c . . . 
+e e 2 2 d f 2 1 1 1 1 b c . . . 
+e e 2 2 d f e e c c c . . . . . 
+b 1 1 d e 2 2 e e c . . . . . . 
+. f f e 2 2 2 2 e . . . . . . . 
+. . f f d d 2 2 f f d d . . . . 
+. . f f d d e e f f d d . . . . 
+. . . f f f f . . . . . . . . . 
+. . e e e f f f . . . . . . . . 
+. . e e e e f f f . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . 2 2 2 2 2 e . . . . 
+. . . . . 2 2 2 2 d 2 2 e . . . 
+. . . . e 2 2 2 2 2 2 2 e . . . 
+. . . . e 2 2 2 2 2 2 2 e . . . 
+. . . . e 2 2 2 2 2 e f f c c . 
+. . . . e e 2 2 e f f f f b c . 
+. . . e e e f e 2 b f f f d c . 
+. . e e 2 2 d f 2 1 1 1 1 b c . 
+. . e e 2 2 d f e e c c c . . . 
+. . b 1 1 d e 2 e e c . . . . . 
+. . f f f f d d f . . . . . . . 
+f f f f f f d d . . . . . . . . 
+f f f . f f f e . . . . . . . . 
+f f . . . . e e e . . . . . . . 
+. . . . . . e e e e . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . 2 2 2 2 2 e . . . . 
+. . . . . 2 2 2 2 d 2 2 e . . . 
+. . . . e 2 2 2 2 2 2 2 e . . . 
+. . . . e 2 2 2 2 2 2 2 e . . . 
+. . . . e 2 2 2 2 2 e f f c c . 
+. . . . e e 2 2 e f f f f b c . 
+. . e e e f e 2 2 b f f f d c . 
+. e e 2 2 d f e 2 1 1 1 1 b c . 
+. e e 2 2 d f e e e c c c . . . 
+. b 1 1 e e 2 2 e e c . . . . . 
+. . f d d 2 2 2 f f f d d . . . 
+e e f d d e e e . f f d d . . . 
+e e e f f f f f . . . . . . . . 
+e e . . . . f f f . . . . . . . 
+. . . . . . f f f f . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . 2 2 2 2 2 e . . . 
+. . . . . . 2 2 2 2 d 2 2 e . . 
+. . . . . e 2 2 2 2 2 2 2 e . . 
+. . . . . e 2 2 2 2 2 2 2 e . . 
+. . . . . e 2 2 2 2 2 e f f c c 
+. . . . . e e 2 2 e f f f f b c 
+. . . e e e f e 2 2 b f f f d c 
+. . e e 2 2 d f e 2 1 1 1 1 b c 
+. . e e 2 2 d f e e e c c c . . 
+. . b 1 1 d e 2 2 e e c . . . . 
+. . . f f f d d 2 2 f d d . . . 
+. . . . f f d d e e f d d . . . 
+. . . . . f f f f f . . . . . . 
+. . . . e e e f f . . . . . . . 
+. . . . e e e e f f . . . . . . 
+`];
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "song1":
+            case "BGM":return hex`0078000408020100001c00010a006400f401640000040000000000000000000000000005000004780000000400012204000800012408000c0001200c001000012410001100012a12001300012a14001500012a16001700012a18001900012a1a001b00012a1c001d00012a1e001f00012a20002100012a22002300012a24002500012a26002700012a28002900012a2a002b00012a2c002d00012a2e002f00012a`;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
--- main.ts
+++ main.ts
@@ -4,96 +4,12 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     }
 })
 let mySprite: Sprite = null
+music.play(music.createSong(assets.song`BGM`), music.PlaybackMode.LoopingInBackground)
 scene.setBackgroundColor(9)
-mySprite = sprites.create(img`
-    . . . . 2 2 2 2 2 e . . . . . . 
-    . . . 2 2 2 2 d 2 2 e . . . . . 
-    . . e 2 2 2 2 2 2 2 e . . . . . 
-    . . e 2 2 2 2 2 2 2 e . . . . . 
-    . . e 2 2 2 2 2 e f f c c . . . 
-    . . e e 2 2 e f f f f b c . . . 
-    . e e e f e 2 b f f f d c . . . 
-    e e 2 2 d f 2 1 1 1 1 b c . . . 
-    e e 2 2 d f e e c c c . . . . . 
-    b 1 1 d e 2 2 e e c . . . . . . 
-    . f f e 2 2 2 2 e . . . . . . . 
-    . . f f d d 2 2 f f d d . . . . 
-    . . f f d d e e f f d d . . . . 
-    . . . f f f f . . . . . . . . . 
-    . . e e e f f f . . . . . . . . 
-    . . e e e e f f f . . . . . . . 
-    `, SpriteKind.Player)
+mySprite = sprites.create(assets.image`Runner`, SpriteKind.Player)
 characterAnimations.loopFrames(
 mySprite,
-[img`
-    . . . . 2 2 2 2 2 e . . . . . . 
-    . . . 2 2 2 2 d 2 2 e . . . . . 
-    . . e 2 2 2 2 2 2 2 e . . . . . 
-    . . e 2 2 2 2 2 2 2 e . . . . . 
-    . . e 2 2 2 2 2 e f f c c . . . 
-    . . e e 2 2 e f f f f b c . . . 
-    . e e e f e 2 b f f f d c . . . 
-    e e 2 2 d f 2 1 1 1 1 b c . . . 
-    e e 2 2 d f e e c c c . . . . . 
-    b 1 1 d e 2 2 e e c . . . . . . 
-    . f f e 2 2 2 2 e . . . . . . . 
-    . . f f d d 2 2 f f d d . . . . 
-    . . f f d d e e f f d d . . . . 
-    . . . f f f f . . . . . . . . . 
-    . . e e e f f f . . . . . . . . 
-    . . e e e e f f f . . . . . . . 
-    `,img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . 2 2 2 2 2 e . . . . 
-    . . . . . 2 2 2 2 d 2 2 e . . . 
-    . . . . e 2 2 2 2 2 2 2 e . . . 
-    . . . . e 2 2 2 2 2 2 2 e . . . 
-    . . . . e 2 2 2 2 2 e f f c c . 
-    . . . . e e 2 2 e f f f f b c . 
-    . . . e e e f e 2 b f f f d c . 
-    . . e e 2 2 d f 2 1 1 1 1 b c . 
-    . . e e 2 2 d f e e c c c . . . 
-    . . b 1 1 d e 2 e e c . . . . . 
-    . . f f f f d d f . . . . . . . 
-    f f f f f f d d . . . . . . . . 
-    f f f . f f f e . . . . . . . . 
-    f f . . . . e e e . . . . . . . 
-    . . . . . . e e e e . . . . . . 
-    `,img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . 2 2 2 2 2 e . . . . 
-    . . . . . 2 2 2 2 d 2 2 e . . . 
-    . . . . e 2 2 2 2 2 2 2 e . . . 
-    . . . . e 2 2 2 2 2 2 2 e . . . 
-    . . . . e 2 2 2 2 2 e f f c c . 
-    . . . . e e 2 2 e f f f f b c . 
-    . . e e e f e 2 2 b f f f d c . 
-    . e e 2 2 d f e 2 1 1 1 1 b c . 
-    . e e 2 2 d f e e e c c c . . . 
-    . b 1 1 e e 2 2 e e c . . . . . 
-    . . f d d 2 2 2 f f f d d . . . 
-    e e f d d e e e . f f d d . . . 
-    e e e f f f f f . . . . . . . . 
-    e e . . . . f f f . . . . . . . 
-    . . . . . . f f f f . . . . . . 
-    `,img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . 2 2 2 2 2 e . . . 
-    . . . . . . 2 2 2 2 d 2 2 e . . 
-    . . . . . e 2 2 2 2 2 2 2 e . . 
-    . . . . . e 2 2 2 2 2 2 2 e . . 
-    . . . . . e 2 2 2 2 2 e f f c c 
-    . . . . . e e 2 2 e f f f f b c 
-    . . . e e e f e 2 2 b f f f d c 
-    . . e e 2 2 d f e 2 1 1 1 1 b c 
-    . . e e 2 2 d f e e e c c c . . 
-    . . b 1 1 d e 2 2 e e c . . . . 
-    . . . f f f d d 2 2 f d d . . . 
-    . . . . f f d d e e f d d . . . 
-    . . . . . f f f f f . . . . . . 
-    . . . . e e e f f . . . . . . . 
-    . . . . e e e e f f . . . . . . 
-    `],
+assets.animation`Running`,
 100,
 characterAnimations.rule(Predicate.MovingRight)
 )
